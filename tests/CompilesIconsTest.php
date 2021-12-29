95a9ef7aa75b43731e589120021290b6eb803d66
3m1n3nc3 <mygames.ng@gmail.com>
2021-12-29 07:45:06 +0100

    Modify Icon prefix to resolve conflict with Blade UI Kit conventions

--- tests/CompilesIconsTest.php
+++ tests/CompilesIconsTest.php
@@ -9,7 +9,7 @@ class CompilesIconsTest extends TestCase
     /** @test */
     public function it_compiles_a_single_anonymous_component()
     {
-        $result = svg('fa6-solid-check')->toHtml();
+        $result = svg('fa6solid-check')->toHtml();
 
         $expected = <<<'SVG'
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
@@ -24,7 +24,7 @@ class CompilesIconsTest extends TestCase
     /** @test */
     public function it_can_add_classes_to_icons()
     {
-        $result = svg('fa6-solid-check', 'w-6 h-6 text-gray-500')->toHtml();
+        $result = svg('fa6solid-check', 'w-6 h-6 text-gray-500')->toHtml();
 
         $expected = <<<'SVG'
         <svg class="w-6 h-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
@@ -39,7 +39,7 @@ class CompilesIconsTest extends TestCase
     /** @test */
     public function it_can_add_styles_to_icons()
     {
-        $result = svg('fa6-solid-check', ['style' => 'fill: #F00'])->toHtml();
+        $result = svg('fa6solid-check', ['style' => 'fill: #F00'])->toHtml();
 
         $expected = <<<'SVG'
         <svg style="fill: #F00" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
