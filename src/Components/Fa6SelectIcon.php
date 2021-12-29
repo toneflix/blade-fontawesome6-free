c079d39d238851023f4e2353b5d2575d71efe1fd
3m1n3nc3 <3m1n3nc3@users.noreply.github.com>
2021-12-29 13:04:10 +0000

    Apply fixes from StyleCI

--- src/Components/Fa6SelectIcon.php
+++ src/Components/Fa6SelectIcon.php
@@ -2,8 +2,8 @@
 
 namespace ToneflixCode\FontAwesome6\Components;
 
-use Illuminate\View\Component;
 use Illuminate\Support\Facades\File;
+use Illuminate\View\Component;
 
 class Fa6SelectIcon extends Component
 {
@@ -11,7 +11,7 @@ class Fa6SelectIcon extends Component
     public $set;
     public $icons;
 
-    protected $ICONS_PATH =     __DIR__.'/../resources/svg/';
+    protected $ICONS_PATH = __DIR__.'/../resources/svg/';
 
     /**
      * Create a new component instance.
@@ -20,9 +20,9 @@ class Fa6SelectIcon extends Component
      */
     public function __construct($selected = null, $set = 'all')
     {
-        $this->selected = $selected; 
-        $this->set = in_array($set, ['all', 'solid', 'regular', 'brands']) ? $set : 'all'; 
-        $this->icons = $this->loadSvg(false, false, $set, $params = ''); 
+        $this->selected = $selected;
+        $this->set = in_array($set, ['all', 'solid', 'regular', 'brands']) ? $set : 'all';
+        $this->icons = $this->loadSvg(false, false, $set, $params = '');
     }
 
     /**
@@ -34,36 +34,30 @@ class Fa6SelectIcon extends Component
     {
         return view('select-form');
     }
- 
- 
-    public function loadSvg($show = null, $link = false, $browse = '', $params = '') 
-    { 
-        $browse      = $browse ? trim($browse, '/') . '/' : 'all';
 
-        $show        = ($show && stripos($show, '.svg') === false) ? "{$show}.svg" : $show;
+    public function loadSvg($show = null, $link = false, $browse = '', $params = '')
+    {
+        $browse = $browse ? trim($browse, '/').'/' : 'all';
+
+        $show = ($show && stripos($show, '.svg') === false) ? "{$show}.svg" : $show;
 
         $icons_array = [];
 
-        foreach (File::files($ICONS_PATH . $browse) as $k => $icon) 
-        { 
-            if ($icon->getExtension() === 'svg') 
-            { 
-                $icons_array[$icon->getFileName()] = $ICONS_PATH . $browse . $icon->getFileName();
+        foreach (File::files($ICONS_PATH.$browse) as $k => $icon) {
+            if ($icon->getExtension() === 'svg') {
+                $icons_array[$icon->getFileName()] = $ICONS_PATH.$browse.$icon->getFileName();
             }
         }
 
-        if ($select && file_exists($icons_array[$show]??'--;--') && !is_dir($icons_array[$show]??'--;--'))
-        {
-            $icons_array = ($link === true) 
-                ? asset('vendor/blade-fontawesome6-free/' . $show) 
+        if ($select && file_exists($icons_array[$show] ?? '--;--') && ! is_dir($icons_array[$show] ?? '--;--')) {
+            $icons_array = ($link === true)
+                ? asset('vendor/blade-fontawesome6-free/'.$show)
                 : file_get_contents($icons_array[$show]);
-        }
-        elseif ($show)
-        {
-            $icons_array = ($link === true) 
-                ? asset('vendor/blade-fontawesome6-free/' . collect($icons_array)->first()) 
+        } elseif ($show) {
+            $icons_array = ($link === true)
+                ? asset('vendor/blade-fontawesome6-free/'.collect($icons_array)->first())
                 : file_get_contents(current($icons_array));
-        } 
+        }
 
         return $icons_array;
     }
